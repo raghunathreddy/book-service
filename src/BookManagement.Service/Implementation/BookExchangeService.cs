9e3354d99d9831b74672d1bd687bdfb03e6f7162
Raghunath <raghunathec@gmail.com>
2024-11-21 19:04:38 +0530

    added code

--- src/BookManagement.Service/Implementation/BookExchangeService.cs
+++ src/BookManagement.Service/Implementation/BookExchangeService.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BookManagement.Service.Implementation
@@ -30,9 +31,53 @@ namespace BookManagement.Service.Implementation
 
         public List<DtoBookExchange> RequesterExchangedBooks(DtoBookExchange searchbook)
         {
+            Random random = new Random();
+
+            // Generate a random integer between 0 and 100 (exclusive)
+            int randomNumber = random.Next(0, 1001);
+            var booktransaction = new DtoBookTransaction()
+            {
+                Book_transactionid= randomNumber,
+                Book_Id = searchbook.book_id,
+                //Book_Name=searchbook.na
+                transaction_date = DateTime.Now,
+                transaction_status="Requested"
+            };
+            SendRequestThansactionserviceAsync(booktransaction);
             var searchbooks = new BookExchange() { requester_id = searchbook.requester_id};
             var result = _bookexchangeReposiroty.ExchangedBook(searchbooks).Result;
             return _mapper.Map<List<DtoBookExchange>>(result);
+        }
+        private async  Task SendRequestThansactionserviceAsync(DtoBookTransaction transactionhistory)
+        {
+
+            // Create an instance of HttpClient
+            using var client = new HttpClient();
+
+            // Define the API URL
+            string url = "http://localhost:8080/api/BookTransaction/AddBookTransaction";
+
+            // Serialize the C# object to JSON
+            string jsonData = JsonSerializer.Serialize(transactionhistory);
+
+            // Create the content for the POST request
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            // Send the POST request
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                // Read the response content
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response Data: " + responseContent);
+            }
+            else
+            {
+                Console.WriteLine("Request failed with status: " + response.StatusCode);
+            }
+
         }
 
         public List<DtoBookExchange> GetAllExchangedBooks()
